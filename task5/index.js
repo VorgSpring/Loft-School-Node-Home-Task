1cd960b73ed08dd134874da27b8d22be0af99fc4
Sergey Pavlov <sergey.pavlov@ostrovok.ru>
2018-06-30 19:27:37 +0300

    created server

---
+++ task5/index.js
@@ -0,0 +1,17 @@
+const express = require(`express`);
+
+const app = express();
+
+app.use('/', express.static(`static`));
+
+app.get(`*`, (_, res) => {
+  return res.redirect('/');
+});
+
+const HOSTNAME = process.env.SERVER_HOST || `127.0.0.1`;
+const PORT = process.env.SERVER_PORT || 3000;
+const SERVER_INFO_MESSAGE = `Server running at http://${HOSTNAME}:${PORT}/`;
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(SERVER_INFO_MESSAGE);
+});
